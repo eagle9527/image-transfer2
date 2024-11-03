cc01d7f8366f286e7a9856ce5342dab64e6b3b0f
eagle9527 <eagle@qq.com>
2024-11-03 13:46:26 +0800

    日志websocket方式显示在前端

--- cmd/image-transfer/main.go
+++ cmd/image-transfer/main.go
@@ -236,7 +236,6 @@ func main() {
 	// 提供 CSS 文件
 	r.GET("/static/css/*filepath", func(c *gin.Context) {
 		filepath := c.Param("filepath")
-		log.Infof("Requested CSS file: %s", filepath)              // 打印请求的文件路径
 		data, err := staticFiles.ReadFile("static/css" + filepath) // 读取嵌入的 CSS 文件
 		if err != nil {
 			log.Errorf("Error reading CSS file: %v", err) // 打印具体错误
@@ -249,7 +248,6 @@ func main() {
 	// 提供 JS 文件
 	r.GET("/static/js/*filepath", func(c *gin.Context) {
 		filepath := c.Param("filepath")
-		fmt.Println("filepath", filepath)
 		data, err := staticFiles.ReadFile("static/js" + filepath) // 读取嵌入的 JS 文件
 		if err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": "JS file not found"})
